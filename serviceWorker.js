89f29120edda4a720358c017c5b4c6c24775a5ca
tgh15 <muhteguhadhiputra15@gmail.com>
2022-09-07 21:14:17 +0800

    Initial commit 2

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,8 @@
 self.addEventListener("install", (e) => {
   console.log("install");
+  caches.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll(["./", "./assets/logo.png", "./assets/tailwind.css"]);
+    })
+  );
 });
