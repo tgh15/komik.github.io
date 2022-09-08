b612e5eacfed98fc21840d8e394480a1078b5e3a
tgh15 <muhteguhadhiputra15@gmail.com>
2022-09-09 06:10:58 +0800

    prev next function

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,27 @@
 let page = document.querySelector("#pages");
+let count = 0;
 
-page.src = "pages/1.png";
+let pages = ["pages/front.png", "pages/1.png"];
+
+window.onload = changePage();
+
+function changePage() {
+  page.src = pages[count];
+}
+
+function prev() {
+  if (count <= 0) {
+    return false;
+  }
+  count -= 1;
+  console.log(count);
+  changePage();
+}
+function next() {
+  if (count >= pages.length - 1) {
+    return false;
+  }
+  count += 1;
+  console.log(count);
+  changePage();
+}
