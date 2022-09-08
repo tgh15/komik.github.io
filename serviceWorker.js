08d83c6fee9c9b2d3a7a5098681de37403e96621
tgh15 <muhteguhadhiputra15@gmail.com>
2022-09-08 21:43:21 +0800

    fix tailwind 3

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,7 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("static").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./assets/logo.png",
-        "https://cdn.tailwindcss.com/3.1.8",
-      ]);
+      return cache.addAll(["./", "./assets/logo.png", "./assets/styles.css"]);
     })
   );
 });
